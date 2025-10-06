0294573bc1f04268bca82e191bc8af481e5089f3
jakebalula <balula@hartford.edu>
2025-10-05 22:05:18 -0400

    Created first bits of database

---
+++ setup.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS passwords;
+
+CREATE DATABASE passwords;
+
+USE passwords;
+
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE sites (
+  
+)
