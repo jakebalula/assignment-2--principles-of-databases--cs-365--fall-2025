5ea27cf0a5e7a273dd877223c44dd6df60461715
jakebalula <balula@hartford.edu>
2025-10-06 11:58:54 -0400

    figuring out sites table

--- setup.sql
+++ setup.sql
@@ -13,5 +13,16 @@ CREATE TABLE users (
 );
 
 CREATE TABLE sites (
-  
+  site_id INT AUTO_INCREMENT PRIMARY KEY,
+  site_name VARCHAR(100) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  encrypted_password ,
+  comment VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
 )
+
+INSERT INTO users(first_name, last_name, username, email) VALUES
+('Jake', 'Balula', 'jbalula', 'balula@example.com'),
+
