b063c1fae2a4c89c138656e93f494b210876a63f
jakebalula <balula@hartford.edu>
2025-10-07 16:20:47 -0400

    making aes encypt

--- setup.sql
+++ setup.sql
@@ -1,8 +1,12 @@
 DROP DATABASE IF EXISTS passwords;
 
-CREATE DATABASE passwords;
+CREATE DATABASE passwords DEFAULT CHARACTER SET utf8mb4;
 
 USE passwords;
+
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('secret password', 512));
+SET @init_vector = RANDOM_BYTES(16);
 
 CREATE TABLE users (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -17,12 +21,24 @@ CREATE TABLE sites (
   site_name VARCHAR(100) NOT NULL,
   url VARCHAR(255) NOT NULL,
   user_id INT NOT NULL,
-  encrypted_password ,
+  encrypted_password VARBINARY(512) NOT NULL,
   comment VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-)
+  init_vector VARBINARY(16) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 INSERT INTO users(first_name, last_name, username, email) VALUES
-('Jake', 'Balula', 'jbalula', 'balula@example.com'),
+('Jake', 'Balula', 'jakebalula', 'jakebalula@gmail.com');
+
+INSERT INTO sites(site_name, url, user_id, encrypted_password, comment, init_vector)
+VALUES
+('Gmail', 'https://mail.google.com', 1,
+ AES_ENCRYPT('password123', @key_str, @init_vector),
+ 'Personal email', @init_vector);
+
+
+
+
+
 
